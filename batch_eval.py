9fcdbecb70c88e468415aae2d8ce5441f0ee7b23
Henry <henrypenghl@gmail.com>
2025-04-10 13:05:11 -0400

    changed batch_eval to use python-chess to allow for timeouts

--- batch_eval.py
+++ batch_eval.py
@@ -1,13 +1,32 @@
 import json
 import time
+import math
 
 from joblib import Parallel, delayed
+import chess
+import chess.engine
 
 from evaluation import evaluate_position
 
 
+def get_eval(game, move):
+    engine = chess.engine.SimpleEngine.popen_uci('stockfish/stockfish-windows-x86-64-avx2.exe')
+    board = chess.Board(game)
+    try:
+        board.push_san(move)
+        evaluation = engine.analyse(board, chess.engine.Limit(time=1))['score']
+        score = evaluation.relative.score(mate_score=math.inf)
+        score /= -100
+        score = 1 / (1 + 10 ** (-score / 4))
+        return score
+    except Exception:
+        return 0
+    finally:
+        engine.quit()
+
+
 def batch_eval(games, moves, n_jobs):
-    ret = Parallel(n_jobs=n_jobs)(delayed(evaluate_position)(g, m) for g, m in zip(games, moves))
+    ret = Parallel(n_jobs=n_jobs)(delayed(get_eval)(g, m) for g, m in zip(games, moves))
     return ret
 
 
@@ -17,12 +36,19 @@ if __name__ == '__main__':
         games = [d['prompt'] for d in data]
         moves = [d['completion'] for d in data]
 
+        for g, m in zip(games, moves):
+            print(g, m)
+
         start = time.time()
-        evals = [evaluate_position(g, m) for g, m in zip(games, moves)]
+        evals = [evaluate_position(g, m)[0] for g, m in zip(games, moves)]
         end = time.time()
-        print(end - start)
+        print(f'\ntime elapsed: {end - start}')
+        for e in evals:
+            print(e)
 
         start = time.time()
         evals = batch_eval(games, moves, 10)
         end = time.time()
-        print(end - start)
+        print(f'\ntime elapsed: {end - start}')
+        for e in evals:
+            print(e)
